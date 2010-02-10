e0d9230f987b73b8d16cb34f340728454427e612
stbrown <stbrown@b93b844c-2574-459d-9dfb-c4b8cb2af1a1>
2010-02-10 14:00:29 +0000

    Added some stuff
    git-svn-id: https://redmine.hindsight.psc.edu/svn/fred/trunk/FRED@93 b93b844c-2574-459d-9dfb-c4b8cb2af1a1

--- VaccineStatus.h
+++ VaccineStatus.h
@@ -12,9 +12,12 @@
 #ifndef _FRED_VACCINESTATUS_H
 #define _FRED_VACCINESTATUS_H
 
-#include <assert>
-#include <stdio>
+#include <stdio.h>
+#include <assert.h>
 #include <iostream>
+#include "Random.h"
+#include "Vaccine.h"
+#include "VaccineDose.h"
 
 class Vaccine_Status {
  public:
@@ -23,14 +26,35 @@ class Vaccine_Status {
     vaccination_day           = -1;
     vaccination_effective_day = -1;
     current_dose              =0;
+    days_to_next_dose         = -1;
   }
+  
+  Vaccine_Status(int day, Vaccine* vacc, int age){
+    assert(day >=0);
+    
+    vaccine = vacc;
+    vaccination_day       = day;
+    double efficacy       = vacc->get_dose(0)->get_efficacy(age);
+    double efficacy_delay = vacc->get_dose(0)->get_efficacy_delay(age);
+    
+    vaccination_effective_day = -1;
+    if(RANDOM() < efficacy){
+      vaccination_effective_day = day+efficacy_delay;
+    }
+    current_dose =0;
+    days_to_next_dose = -1;
+    if(vacc->get_number_doses() > 1){
+      days_to_next_dose = day + vacc->get_dose(0)->get_days_between_doses();
+    }
+  }
+  
   
   // Access Members
   int get_vaccination_day(void)           { return vaccination_day; }
   int get_vaccination_effective_day(void) { return vaccination_effective_day; }
   Vaccine* get_vaccine(void)              { return vaccine; }
   int get_current_dose(void)              { return current_dose; }
-  
+  int get_days_to_next_dose(void)         { return days_to_next_dose; }
   // Modifiers
   void set_vaccination_day(int day) { 
     assert(day >= 0);
@@ -42,17 +66,18 @@ class Vaccine_Status {
       cout << "\nWARNING! Vaccination Status, setting vaccine day of someone who has already been vaccinated";
     }
   }
-  
-  void set_vaccine_effective_day(void) {
-    assert(vaccination_day > -1);
-    int vaccination_delay = vaccine
-    
+ 
+  //Utility Functions
+  void print(void){
+    cout << "\nVaccine Status";
+}
       
  private:
   int vaccination_day;             // On which day did you get the vaccine
   int vaccination_effective_day;   // On which day is the vaccine effective
   Vaccine* vaccine;                // Which vaccine did you take
   int current_dose;
+  int days_to_next_dose;
 };
 
 #endif
